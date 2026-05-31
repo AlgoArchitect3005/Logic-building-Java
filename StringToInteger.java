43dfeddeb9fdd77ef7aea11e387a2bc036520a52
Yash Gupta <guptayash.programmer@gmail.com>
2026-06-01 00:23:14 +0530

    Add initial implementation of StringToInteger class with myAtoi method

---
+++ StringToInteger.java
@@ -0,0 +1,21 @@
+public class StringToInteger {
+    public static void main(String[] args) {
+    }
+}
+
+class Solution {
+    public int myAtoi(String s) {
+        int i = 0;
+        int sign = 1;
+        int result = 0;
+
+        // spaces skip
+
+        // sign check
+
+        // digits read
+
+        // return sign * result
+        return 0;
+    }
+}
