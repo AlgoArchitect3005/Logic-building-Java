90c684bd42307f19ac52ac90f0b46267f0d522c3
Yash Gupta <guptayash.programmer@gmail.com>
2026-06-01 02:01:31 +0530

    Implement myAtoi method in Solution class to convert string to integer

--- StringToInteger.java
+++ StringToInteger.java
@@ -1,5 +1,9 @@
 public class StringToInteger {
     public static void main(String[] args) {
+        Solution solution = new Solution();
+        String s = "   -42cba";
+        int result = solution.myAtoi(s);
+        System.out.println(result); // Output: -42
     }
 }
 
@@ -8,14 +12,35 @@ class Solution {
         int i = 0;
         int sign = 1;
         int result = 0;
+        int n = s.length();
 
         // spaces skip
+        while (i < n && s.charAt(i) == ' ') {
+                i++;
+        }
 
         // sign check
+        if (i < n && (s.charAt(i) == '+' || s.charAt(i) == '-')){
+        sign = (s.charAt(i) == '+') ? 1 : -1;
+                i++;
+            }
 
-        // digits read
-
+            // digits read
+             while(i < n && Character.isDigit(s.charAt(i))){
+                int digit = Character.getNumericValue(s.charAt(i));
+                // check for overflow
+                if (result > (Integer.MAX_VALUE - digit) / 10) {
+                    return (sign == 1) ? Integer.MAX_VALUE : Integer.MIN_VALUE;
+                }
+                
+                else{
+                    result = result * 10 + digit;
+                    i++;
+                }
+             }
+            
+        
         // return sign * result
-        return 0;
+        return sign * result;
     }
 }
